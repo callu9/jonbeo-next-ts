17bc099f4ff021b2b4d348d845f466680871b2d9
callu9 <callu_9ine@naver.com>
2025-09-18 19:25:25 +0900

    chore: set tailwind prettier and mock service worker

--- src/mocks/server.ts
+++ src/mocks/server.ts
@@ -2,5 +2,4 @@ import { setupServer } from "msw/node";
 import { portfolioHandler } from "./handler/portfolio";
 import { stocksHandler } from "./handler/stocks";
 
-// MSW 서버를 설정합니다.
 export const server = setupServer(...portfolioHandler, ...stocksHandler);
