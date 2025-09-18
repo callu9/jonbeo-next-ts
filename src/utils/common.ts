e42545200fb2f14a6cbf13d0ee9919d3ac8e48a5
callu9 <callu_9ine@naver.com>
2025-09-18 19:27:28 +0900

    feat: add common util functions for className and formatting number

---
+++ src/utils/common.ts
@@ -0,0 +1,12 @@
+const DOLLAR_TO_WON = 1_385.9;
+
+const changeDollarToWon = (dollar: number) => Math.floor(dollar * DOLLAR_TO_WON);
+
+export const formatNumberWithComma = (price: string | number) => {
+  return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
+
+export const formatWon = (amount: number) =>
+  `${formatNumberWithComma(changeDollarToWon(amount))}원`;
+
+export const formatDollar = (amount: number) => `$${formatNumberWithComma(amount)}`;
